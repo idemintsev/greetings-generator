d215ef86f3dfd88096e1515bea53e316725d038b
idemintsev <56403070+idemintsev@users.noreply.github.com>
2021-08-17 18:43:12 +0300

    Update engine.py
    Добавлена БД, удалено чтение из файлов, изменены методы класса (для работы с БД).

--- engine.py
+++ engine.py
@@ -1,35 +1,30 @@
 # -*- coding: utf-8 -*-
+
 import os
-from collections import defaultdict
+import sqlite3
 from random import randint
+
+DATABASE = 'phrases.db'
 
 
 class GreetingSpeechMaker:
     """
     Use python3.7
-    
-    Создает поздравления с днем рождения и сохраняет в файл '.txt'.
+
+    Создает поздравления с днем рождения и сохраняет в файл '*.txt'.
 
     Принимает параметры:
     speeches_quantity - (int) количество поздравлений, которое необходимо создать.
     path - (str) путь для сохранения файла с поздравлениями.
     file_name - (str) имя файла с поздравлениями.
 
-    Атрибуты:
-    self.files_with_phrase_snippets - хранит названия файлов, содержащих фразы для поздравлений и количество строк в
-                                      этих файлах. При инициализации количество строк не задано и добавляется после
-                                      запуска метода self.string_counter.
-
     Методы:
-    string_counter - считает количество строк в файлах 'first_part_of_speech.txt' и 'last_part_of_speech.txt'
-                     и записывает результат в self.string_quantity_in_files.
+    get_db_row_quantity - считает коилчество записей в БД, возвращает кортеж с количеством записей в первой и второй
+                          таблицах БД.
 
-    file_reader - случайным образом определяет номер строки с текстом для поздравления и возвращает эту строку.
-
-    make_speech - добавляет в self.text_for_saving строку, возвращенную file_reader.
+    make_speech - достает из БД строки и добавляет их в self.text_for_saving.
 
     save_final_speech - сохраняет текст поздравления в файл на компьютере.
-
     """
 
     def __init__(self, speeches_quantity=None, path=None, file_name=None):
@@ -39,44 +34,50 @@ class GreetingSpeechMaker:
             self.path = os.path.join(os.path.dirname(__file__), self.file_name)
         else:
             self.path = os.path.join(os.path.abspath(path), self.file_name)
-        self.files_with_phrase_snippets = defaultdict(list)
-        self.files_with_phrase_snippets = {
-            'first_file': ['first_part_of_speech.txt', ],
-            'second_file': ['last_part_of_speech.txt', ]
-        }
-        self.text_for_saving = []
+        self.text_for_saving = list()
 
-    def string_counter(self):
-        for key, value in self.files_with_phrase_snippets.items():
-            _file = self.files_with_phrase_snippets[key][0]
-            with open(_file, encoding='utf8') as ff:
-                for number, string in enumerate(ff):
-                    lines_quantity = number
-            value.append(lines_quantity)
+    def get_db_row_quantity(self) -> tuple:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        try:
+            data_from_first_db = cursor.execute('SELECT COUNT(*) FROM first_part_of_speech')
+            result_from_first_db = int([el[0] for el in data_from_first_db][0])
+            data_from_second_db = cursor.execute('SELECT COUNT(*) FROM second_part_of_speech')
+            result_from_second_db = int([el[0] for el in data_from_second_db][0])
+            result = tuple([result_from_first_db, result_from_second_db])  # tuple(int, int)
+        finally:
+            cursor.close()
+        return result
 
-    def file_reader(self, key):
-        _file = self.files_with_phrase_snippets[key][0]
-        with open(file=_file, encoding='utf8') as ff:
-            line_number = randint(2, self.files_with_phrase_snippets[key][1])
-            for number, line in enumerate(ff):
-                if number == line_number:
-                    return line[:-1]
+    def make_speech(self, first_phrase_number: int, second_phrase_number: int) -> None:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        try:
+            # Достаем фразу из первой таблицы БД
+            data_from_db = cursor.execute(
+                'SELECT text FROM first_part_of_speech WHERE id=?', (first_phrase_number,))
+            text = [t[0] for t in data_from_db][0]  # Получаем str
+            self.text_for_saving.append(text)
+            # Достаем фразу из второй таблицы БД
+            data_from_db = cursor.execute(
+                'SELECT text FROM second_part_of_speech WHERE id=?', (second_phrase_number,))
+            text = [t[0] for t in data_from_db][0]  # Получаем str
+            self.text_for_saving.append(text)
+        finally:
+            cursor.close()
 
-    def make_speech(self):
-        for key, value in self.files_with_phrase_snippets.items():
-            phrase = self.file_reader(key)
-            self.text_for_saving.append(phrase)
-
-    def save_final_speech(self):
+    def save_final_speech(self) -> None:
         final_text = f'С днем рождения!\n{self.text_for_saving[0]}\n{self.text_for_saving[1]}\n\n'
         self.text_for_saving.clear()
         with open(file=self.path, mode='a', encoding='utf8', ) as file:
             file.write(final_text)
 
     def run(self):
-        self.string_counter()
+        row_quantity_in_first_db, row_quantity_in_second_db = self.get_db_row_quantity()
         for _speech in range(self.speeches_quantity):
-            self.make_speech()
+            first_phrase_number = randint(1, row_quantity_in_first_db)
+            second_phrase_number = randint(1, row_quantity_in_second_db)
+            self.make_speech(first_phrase_number, second_phrase_number)
             self.save_final_speech()
         print(f'Ваше поздравление готово и сохранено в файл "{self.file_name}"!'
               f'\nПолный путь к файлу {self.path}')
